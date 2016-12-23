5468e9a909072b6fda9ae3ff1ad03179396058a9
Victor Aprea <victor.aprea@wickeddevice.com>
2016-12-23 11:01:14 -0500

    add test case

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <string.h>
 #include <stdio.h>
- 
+
 int luhn(const char* cc)
 {
 	const int m[] = {0,2,4,6,8,1,3,5,7,9}; // mapping for rule 3
 	int i, odd = 1, sum = 0;
- 
+
 	for (i = strlen(cc); i--; odd = !odd) {
 		int digit = cc[i] - '0';
 		sum += odd ? digit : m[digit];
 	}
- 
+
 	return sum % 10 == 0;
 }
 
 int main(int argc, char * argv){
 
+  // test input "49927398716"
+
 	return 0;
 }
-
